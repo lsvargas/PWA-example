bc43cd5aee1c8c79e0600932b2c74a4ff2b3d123
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-09-24 00:37:41 -0300

    delete import config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,9 @@
-import FB_CONGIG from '../src/config/config';
-
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 
 firebase.initializeApp({
-  messagingSenderId: FB_CONGIG.messagingSenderId,
+  messagingSenderId: '32474194765',
 });
 
 const messaging = firebase.messaging();
