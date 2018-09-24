74b9f9c1387276dcfbecbca995bf5219124b8638
lsvargas <lsvargas@uc.cl>
2018-09-24 09:52:03 -0300

    production build

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -45,7 +45,7 @@ function createDB() {
 }
 
 export default function register() {
-  if (process.env.NODE_ENV === 'development' && 'serviceWorker' in navigator && 'PushManager' in window) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator && 'PushManager' in window) {
     // The URL constructor is available in all browsers that support SW.
     console.log('service worker and push is suported');
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
